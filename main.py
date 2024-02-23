7108aad2198e82bce515b84836438612f72a6677
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-02-23 18:50:35 -0500

    algoritmo para calcular el factorial de un número

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-numPrimo = int(input("Ingrese el número para averiguar si es primo: "))
-divisorPrimo = numPrimo
-contarCantidadMultiplo = 0;
-while int(divisorPrimo) >= 1:
-    if int(numPrimo) % int(divisorPrimo) == 0:
-        contarCantidadMultiplo+=1
-    divisorPrimo -= 1
-if contarCantidadMultiplo == 2:
-    print("El número " + str(numPrimo) + " es primo")
-else:
-    print("El número " + str(numPrimo) + " no es primo")
+numFactorial = int(input("Ingrese el número para conocer su factorial: "))
+acumularResultadoFactorial = 1
+while numFactorial>=1:
+    acumularResultadoFactorial = acumularResultadoFactorial * numFactorial
+    numFactorial-=1
+print("El resultado del factorial es: " + str(acumularResultadoFactorial))
