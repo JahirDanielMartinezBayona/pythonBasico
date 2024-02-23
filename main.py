19c2b4b466421cee689d55df14104f9c71787714
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-02-23 18:55:15 -0500

    algoritmo para determinar si un número es par o impar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numFactorial = int(input("Ingrese el número para conocer su factorial: "))
-acumularResultadoFactorial = 1
-while numFactorial>=1:
-    acumularResultadoFactorial = acumularResultadoFactorial * numFactorial
-    numFactorial-=1
-print("El resultado del factorial es: " + str(acumularResultadoFactorial))
+numParImpar = int(input("Ingrese el número para saber si es par o impar: "))
+if numParImpar % 2 == 0:
+    print("El número ingresado es par")    
+else:
+    print("El número ingresado es impar")
