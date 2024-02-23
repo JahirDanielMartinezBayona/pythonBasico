74d74fbd9770b2736b385d64ce31895172c25c14
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-02-23 18:27:10 -0500

    algoritmo para verificar si un número es primo

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-num1 = input("Ingrese el número 1: ")
-num2 = input("Ingrese el número 2: ")
-suma = int(num1) + int(num2)
-print(suma)
+numPrimo = int(input("Ingrese el número para averiguar si es primo: "))
+divisorPrimo = numPrimo
+contarCantidadMultiplo = 0;
+while int(divisorPrimo) >= 1:
+    if int(numPrimo) % int(divisorPrimo) == 0:
+        contarCantidadMultiplo+=1
+    divisorPrimo -= 1
+if contarCantidadMultiplo == 2:
+    print("El número " + str(numPrimo) + " es primo")
+else:
+    print("El número " + str(numPrimo) + " no es primo")
