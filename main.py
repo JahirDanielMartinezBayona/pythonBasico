c58d675e77618f80e8ed79c8e1c69405519aadda
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-02-23 19:00:58 -0500

    algoritmo para calcular el área de un triángulo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numParImpar = int(input("Ingrese el número para saber si es par o impar: "))
-if numParImpar % 2 == 0:
-    print("El número ingresado es par")    
-else:
-    print("El número ingresado es impar")
+numBase = float(input("Ingrese la cantidad de la base del triángulo: "))
+numAltura = float(input("Ingrese la cantidad de la altura del triángulo: "))
+resultadoTriangulo = (numBase * numAltura)/2
+print("El resultado del área del triángulo es: " + str(resultadoTriangulo))
+
