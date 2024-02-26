0768601638c3c74cd0340f81f59e849551d58eda
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-02-26 14:08:52 -0500

    algoritmo para convertir grados celsius a fahrenheit

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-print("LA TABLA DE MULTIPLICAR QUE TANTO TE GUSTA")
-multiploBase = input("Ingresa el múltiplo base de la tabla: ")
-multiploMaximo = input("Ingresa el múltiplo hasta dónde puede ser multiplicado la tabla: ")
-print("La tabla a multiplicar es: "+ str(multiploBase))
-
-almacenarDatoMultiplo = int(multiploMaximo)+1
-for postearMultiplo in range(1,almacenarDatoMultiplo):
-    resultado = int(multiploBase) * int(postearMultiplo)
-    print(f"{multiploBase} x {postearMultiplo} = {resultado}")
+celsiusDato = input("Ingresa el valor en Celsius para hallar los grados en Farenheit: ")
+farenheit = (float(celsiusDato) * 9/5) + 32
+print("Los grados Celsius en Farenheit son: "+str(farenheit)+"°F")
