6cc1c2968c5488506be6b9ae11a7edccca92dab5
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-02-26 13:07:21 -0500

    algoritmo para calcular el área de un círculo

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-numCantidadPromedio = int(input("Ingrese la cantidad de números a calcular su promedio: "))
-mostrarNumeroElemento = 1
-posicionNumeroElemento = 0
-arregloPromedioNumeros = []
-sumaPromedio = 0
-resultadoPromedio = 0
-while numCantidadPromedio > 0:
-    arregloPromedioNumeros.append((input("Ingresa el número " + str(mostrarNumeroElemento) + ": ")))
-    mostrarNumeroElemento += 1
-    numCantidadPromedio -= 1
-
-while posicionNumeroElemento <= (len(arregloPromedioNumeros)-1):
-    sumaPromedio = float(sumaPromedio) + float(arregloPromedioNumeros[posicionNumeroElemento])
-    posicionNumeroElemento += 1
-resultadoPromedio= sumaPromedio/(len(arregloPromedioNumeros))
-print("La cantidad de números evaluados fueron: "+str(len(arregloPromedioNumeros)))
-print("El promedio de los números insertados es: "+str(resultadoPromedio))
+import math
+radio = input("Ingresa el radio para el cálculo del área del círculo: ")
+resultadoAreaCirculo =  math.pi * float(radio) ** 2
+print("El resultado del área del círculo es: "+ str(resultadoAreaCirculo))
